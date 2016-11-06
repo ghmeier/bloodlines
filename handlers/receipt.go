bb7fe29c47616fc22fe94fb362a30ae5aecf7ea0
garret.meier <garret.meier@bunchball.com>
2016-11-05 20:26:10 -0500

    moved to helpers

--- handlers/receipt.go
+++ handlers/receipt.go
@@ -1,13 +1,11 @@
 package handlers
 
 import (
-	"fmt"
-	"strings"
-
 	"gopkg.in/gin-gonic/gin.v1"
 
 	"github.com/ghmeier/bloodlines/models"
 	"github.com/ghmeier/bloodlines/gateways"
+	"github.com/ghmeier/bloodlines/helpers"
 )
 
 type ReceiptIfc interface {
@@ -17,47 +15,36 @@ type ReceiptIfc interface {
 }
 
 type Receipt struct {
-	sql *gateways.Sql
+	helper *helpers.Receipt
 }
 
 func NewReceipt(sql *gateways.Sql) ReceiptIfc {
-	return &Receipt{sql: sql}
+	return &Receipt{helper: helpers.NewReceipt(sql)}
 }
 
 func (r *Receipt) Send(ctx *gin.Context) {
 	var json models.Receipt
 	err := ctx.BindJSON(&json)
 	if err != nil {
-		fmt.Println(err.Error())
 		ctx.JSON(400, errResponse("Invalid vals or contentId"))
 		return
 	}
 
 	receipt := models.NewReceipt(json.Values, json.ContentId)
-	err = r.sql.Modify(
-		"INSERT INTO receipt (id, ts, vals, sendState, contentId) VALUES (?, ?, ?, ?, ?)",
-		receipt.Id,
-		receipt.Created,
-		strings.Join(receipt.Values,","),
-		receipt.SendState,
-		receipt.ContentId,
-	)
+
+	err = r.helper.Insert(receipt)
 	if err != nil {
 		ctx.JSON(500, errResponse(err.Error()))
 		return
 	}
+
+	// SEND SOMETHING
 
 	ctx.JSON(200, gin.H{"Data": receipt})
 }
 
 func (r *Receipt) ViewAll(ctx *gin.Context) {
-	rows, err := r.sql.Select("Select id, ts, vals, sendState, contentId from receipt")
-	if err != nil {
-		ctx.JSON(500, errResponse(err.Error()))
-		return
-	}
-
-	receipts, err := models.ReceiptFromSql(rows)
+	receipts, err := r.helper.GetReceipts()
 	if err != nil {
 		ctx.JSON(500, errResponse(err.Error()))
 		return
@@ -73,29 +60,10 @@ func (r *Receipt) View(ctx *gin.Context) {
 		return
 	}
 
-	receipt, err := r.GetReceiptById(id)
+	receipt, err := r.helper.GetReceiptById(id)
 	if err != nil {
 		ctx.JSON(500, errResponse(err.Error()))
 	}
 
 	ctx.JSON(200, gin.H{"data":receipt})
-}
-
-
-func (r *Receipt) GetReceiptById(id string) (*models.Receipt, error) {
-	rows, err := r.sql.Select("SELECT id, ts, vals, sendState, contentId FROM receipt WHERE id=?", id)
-	if err != nil {
-		return nil, err
-	}
-
-	receipts, err := models.ReceiptFromSql(rows)
-	if err != nil {
-		return nil, err
-	}
-	return receipts[0], nil
-}
-
-func (r *Receipt) SetSendState(id string, state models.Status) error {
-	err := r.sql.Modify("UPDATE receipt SET sendState=? where id=?", state, id)
-	return err
 }
--- helpers/receipt.go
+++ helpers/receipt.go
@@ -1,9 +1,9 @@
 package helpers
 
 import (
-//	"strings"
+	"strings"
 
-//	"github.com/ghmeier/bloodlines/models"
+	"github.com/ghmeier/bloodlines/models"
 	"github.com/ghmeier/bloodlines/gateways"
 )
 
@@ -13,4 +13,48 @@ type Receipt struct{
 
 func NewReceipt(sql *gateways.Sql) *Receipt {
 	return &Receipt{baseHelper: &baseHelper{sql: sql}}
+}
+
+func (r *Receipt) Insert(receipt *models.Receipt) error {
+	err := r.sql.Modify(
+		"INSERT INTO receipt (id, ts, vals, sendState, contentId) VALUES (?, ?, ?, ?, ?)",
+		receipt.Id,
+		receipt.Created,
+		strings.Join(receipt.Values,","),
+		receipt.SendState,
+		receipt.ContentId,
+	)
+	return err
+}
+
+func (r *Receipt) GetReceipts() ([]*models.Receipt, error) {
+	rows, err := r.sql.Select("Select id, ts, vals, sendState, contentId from receipt")
+	if err != nil {
+		return nil, err
+	}
+
+	receipts, err := models.ReceiptFromSql(rows)
+	if err != nil {
+		return nil, err
+	}
+
+	return receipts, nil
+}
+
+func (r *Receipt) GetReceiptById(id string) (*models.Receipt, error) {
+	rows, err := r.sql.Select("SELECT id, ts, vals, sendState, contentId FROM receipt WHERE id=?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	receipts, err := models.ReceiptFromSql(rows)
+	if err != nil {
+		return nil, err
+	}
+	return receipts[0], nil
+}
+
+func (r *Receipt) SetSendState(id string, state models.Status) error {
+	err := r.sql.Modify("UPDATE receipt SET sendState=? where id=?", state, id)
+	return err
 }
