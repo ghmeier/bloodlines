89f1ace50be3620a27b20dce720a043c70db8980
garret.meier <garret.meier@bunchball.com>
2016-12-07 13:58:13 -0600

    adding router

---
+++ router/router_trigger_test.go
@@ -0,0 +1,253 @@
+package router
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ghmeier/bloodlines/models"
+
+	"github.com/pborman/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"gopkg.in/gin-gonic/gin.v1"
+)
+
+func TestTriggerViewAllSuccess(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+
+	b, mock := mockTrigger()
+	mock.On("GetAll", 0, 20).Return(make([]*models.Trigger, 0), nil)
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/api/trigger", nil)
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(200, w.Code)
+}
+
+func TestTriggerViewAllParams(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+
+	b, mock := mockTrigger()
+	mock.On("GetAll", 20, 100).Return(make([]*models.Trigger, 0), nil)
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/api/trigger?offset=20&limit=100", nil)
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(200, w.Code)
+}
+
+func TestTriggerViewAllFail(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+
+	b, mock := mockTrigger()
+	mock.On("GetAll", 0, 20).Return(nil, fmt.Errorf("some error"))
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/api/trigger", nil)
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(500, w.Code)
+}
+
+func TestTriggerNewSuccess(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+	trigger := models.Trigger{
+		ContentID: uuid.NewUUID(),
+		Key:       "test_key",
+		Params:    make([]string, 0),
+	}
+
+	b, tMock := mockTrigger()
+	tMock.On("Insert", mock.AnythingOfType("*models.Trigger")).Return(nil)
+
+	s, _ := json.Marshal(trigger)
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/api/trigger", bytes.NewReader(s))
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(200, w.Code)
+}
+
+func TestTriggerNewFail(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+	trigger := models.Trigger{
+		ContentID: uuid.NewUUID(),
+		Key:       "test_key",
+		Params:    make([]string, 0),
+	}
+
+	b, tMock := mockTrigger()
+	tMock.On("Insert", mock.AnythingOfType("*models.Trigger")).Return(fmt.Errorf("some error"))
+
+	s, _ := json.Marshal(trigger)
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/api/trigger", bytes.NewReader(s))
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(500, w.Code)
+}
+
+func TestTriggerNewInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+	b, _ := mockTrigger()
+
+	s := []byte("{\"contentId\": \"invalid-id\"}")
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/api/trigger", bytes.NewReader(s))
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(400, w.Code)
+}
+
+func TestTriggerViewSuccess(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+
+	b, mock := mockTrigger()
+	mock.On("GetByKey", "test_key").Return(&models.Trigger{}, nil)
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/api/trigger/test_key", nil)
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(200, w.Code)
+}
+
+func TestTriggerViewFail(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+
+	b, mock := mockTrigger()
+	mock.On("GetByKey", "test_key").Return(nil, fmt.Errorf("some error"))
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/api/trigger/test_key", nil)
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(500, w.Code)
+}
+
+func TestTriggerUpdateSuccess(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+	trigger := models.Trigger{
+		ID:        uuid.NewUUID(),
+		ContentID: uuid.NewUUID(),
+		Params:    make([]string, 0),
+		Key:       "test_key",
+	}
+
+	b, tMock := mockTrigger()
+	tMock.On("Update", trigger.Key, trigger.ContentID, trigger.Params).Return(nil)
+
+	s, _ := json.Marshal(trigger)
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("PUT", "/api/trigger/test_key", bytes.NewReader(s))
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(200, w.Code)
+}
+
+func TestTriggerUpdateInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+
+	b, _ := mockTrigger()
+
+	s := []byte("{\"contentId\": \"invalid-id\"}")
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("PUT", "/api/trigger/test_key", bytes.NewReader(s))
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(400, w.Code)
+}
+
+func TestTriggerUpdateFail(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+	trigger := models.Trigger{
+		ID:        uuid.NewUUID(),
+		ContentID: uuid.NewUUID(),
+		Key:       "test_key",
+		Params:    make([]string, 0),
+	}
+
+	b, tMock := mockTrigger()
+	tMock.On("Update", trigger.Key, trigger.ContentID, trigger.Params).Return(fmt.Errorf("some error"))
+
+	s, _ := json.Marshal(trigger)
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("PUT", "/api/trigger/test_key", bytes.NewReader(s))
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(500, w.Code)
+}
+
+func TestTriggerDeleteSuccess(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+	key := "test_key"
+
+	b, tMock := mockTrigger()
+	tMock.On("Delete", key).Return(nil)
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("DELETE", "/api/trigger/test_key", nil)
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(200, w.Code)
+}
+
+func TestTriggerDeleteFail(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+	key := "test_key"
+
+	b, tMock := mockTrigger()
+	tMock.On("Delete", key).Return(fmt.Errorf("some error"))
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("DELETE", "/api/trigger/test_key", nil)
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(500, w.Code)
+}
+
+func TestTriggerActivateSuccess(t *testing.T) {
+	assert := assert.New(t)
+
+	gin.SetMode(gin.TestMode)
+
+	b, _ := mockTrigger()
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/api/trigger/test_key/activate", nil)
+	b.router.ServeHTTP(w, r)
+
+	assert.Equal(200, w.Code)
+}
