170451e4f8d198c128f7efaf6a7d095d68f20fb5
garret.meier <garret.meier@bunchball.com>
2017-01-27 09:28:46 -0600

    Comment gateways.go

--- gateways/gateways.go
+++ gateways/gateways.go
@@ -7,6 +7,16 @@ import (
 	"net/http"
 )
 
+/* ServiceResponse is used to unmarshal data from expresso services.
+It can be used in conjunction with the following methods like this:
+
+data, _ := ServiceGet(url)
+var c []*models.Content
+err := json.Unmarshal(data, &c)
+
+Since ServiceGet returns data as a []byte, we can unmarshal it
+to whatever is needed in the calling method. Here, its []*models.Content
+*/
 type ServiceResponse struct {
 	Msg     string `json:"message"`
 	Data    []byte `json:"data"`
@@ -14,6 +24,7 @@ type ServiceResponse struct {
 	Success bool   `json:"success"`
 }
 
+/*ServiceGet makes a request using c to the given url, returning the 'data' field */
 func ServiceGet(c *http.Client, url string) ([]byte, error) {
 	resp, err := c.Get(url)
 	if err != nil {
@@ -29,6 +40,8 @@ func ServiceGet(c *http.Client, url string) ([]byte, error) {
 	return data, nil
 }
 
+/*ServicePost makes a POST request using c to the given url, with data as a json
+  payload returning the 'data' field */
 func ServicePost(c *http.Client, url string, data []byte) ([]byte, error) {
 	r := bytes.NewBuffer(data)
 	resp, err := c.Post(url, "application/json", r)
@@ -45,6 +58,8 @@ func ServicePost(c *http.Client, url string, data []byte) ([]byte, error) {
 	return rData, nil
 }
 
+/*ServicePut makes a PUT request using c to the given url, with data as a json
+  payload returning the 'data' field */
 func ServicePut(c *http.Client, url string, data []byte) ([]byte, error) {
 	r := bytes.NewBuffer(data)
 	req, err := http.NewRequest("PUT", url, r)
@@ -66,6 +81,8 @@ func ServicePut(c *http.Client, url string, data []byte) ([]byte, error) {
 	return rData, nil
 }
 
+/*ServiceDelete makes a DELETE request using c to the given url, with data as a json
+  payload returning the 'data' field */
 func ServiceDelete(c *http.Client, url string, data []byte) ([]byte, error) {
 	r := bytes.NewBuffer(data)
 	req, err := http.NewRequest("DELETE", url, r)
